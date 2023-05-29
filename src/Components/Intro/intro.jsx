9c839ebe46518e28c53d7601af3a75a386fb4bac
Lewis Hill <lewis.james.hill@outlook.com>
2023-05-30 00:05:53 +1000

    intro

--- src/Components/Intro/intro.jsx
+++ src/Components/Intro/intro.jsx
@@ -1,23 +1,50 @@
-import React from "react";
+"use client";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import { MailtoButton } from "../UI/Button";
 import styles from "./intro.module.css";
+import Image from "next/image";
+import portrait from "../../../public/portrait.svg";
+import styled from "styled-components";
+
+const MailtoButtonCustom = styled(MailtoButton)`
+    align-self: flex-end;
+    transform: translateX(-4px);
+`;
 
 export default function Intro() {
+    const ref = useRef(null);
+    const [width, setWidth] = useState(100);
+    useLayoutEffect(() => {
+        setWidth(ref.current.offsetWidth);
+    }, []);
     return (
-        <div>
-            <div>
+        <div className={styles.intro}>
+            <div className={styles.left}>
                 <h1 className={styles.header}>
-                    LEWIS IS A{" "}
+                    Lewis is a{" "}
                     <span className={styles.headerHighlight}>
-                        FULL-STACK DEVELOPER
+                        full-stack developer
                     </span>
                 </h1>
-                <p></p>
-                <MailtoButton href="mailto:">Contact Me!</MailtoButton>
+                <div className={styles.descriptionContainer}>
+                    <p className={styles.desc}>
+                        He crafts responsive websites where technologies meet
+                        creativity
+                    </p>
+                    <MailtoButtonCustom href="mailto:">
+                        Contact Me!
+                    </MailtoButtonCustom>
+                </div>
             </div>
-            <div>
-                <div></div>
-                <div></div>
+            <div className={styles.right}>
+                <Image src={portrait} width="100%" height="100%" ref={ref} />
+
+                <div
+                    className={styles.imageFooter}
+                    style={{ width: `${width}px` }}
+                >
+                    Currently working on <b>Portfolio</b>
+                </div>
             </div>
         </div>
     );
--- src/Components/UI/Button.jsx
+++ src/Components/UI/Button.jsx
@@ -2,8 +2,16 @@ import styled from "styled-components";
 
 export const Button = styled.button`
     border: 1px solid var(--accent-color);
+    padding: 8px 16px;
+    gap: 10px;
+    max-width: 150px;
+    text-align: center;
 `;
 
 export const MailtoButton = styled.a`
     border: 1px solid var(--accent-color);
+    text-align: center;
+    padding: 8px 16px;
+    gap: 10px;
+    max-width: 150px;
 `;
