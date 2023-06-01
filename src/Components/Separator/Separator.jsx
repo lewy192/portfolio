f7871b8fbc828bfefbfd09eda2932a9489e6ef1d
Lewis Hill <lewis.james.hill@outlook.com>
2023-06-02 08:36:23 +1000

    updates to quote sizing responsiveness

---
+++ src/Components/Separator/Separator.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./separator.module.css";
+
+export const Separator = ({ title, dividerLength }) => {
+    return (
+        <div className={styles.separator}>
+            <h2></h2>
+            <div></div>
+        </div>
+    );
+};
