e73c2e22e51227f07bda3e0a76d786ff1d062009
Lewis Hill <lewis.james.hill@outlook.com>
2023-06-01 21:34:00 +1000

    add quote

--- src/Components/quote/quote.jsx
+++ src/Components/quote/quote.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import styles from "./quote.module.css";
+import quote from "../../../public/quote.svg";
 
 export const Quote = (props) => {
-    return <div></div>;
+    return (
+        <figure className={styles.quoteContainer}>
+            <quote className={styles.quote}>
+                Development is like building lego with no piece limit
+            </quote>
+            <figcaption className={styles.caption}>- Lewis hill</figcaption>
+        </figure>
+    );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Intro from "../Components/Intro/intro";
+import { Quote } from "@/Components/quote/quote";
 
 export default function Home() {
     return (
         <main className={styles.main}>
             <Intro />
+            <Quote />
         </main>
     );
 }
